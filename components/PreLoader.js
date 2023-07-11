dc1ee61368bab2339ea3f4b35dd81c6f42ab4fbf
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-07-10 20:39:53 -0400

    Initial commit

---
+++ components/PreLoader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./preloader.css";
+const PreLoader = () => {
+  return (
+    <div className="preloader">
+      <div classname="texts-container">
+        <span>Please,</span>
+        <span>Hire,</span>
+        <span>Me! </span>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PreLoader from "../components/PreLoader";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <PreLoader />
+
+      <div className="App">
+        <h1>Hello gang</h1>
+      </div>
+    </>
+  );
+}
